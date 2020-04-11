8e04f07bb59010047ecfc91aa0a5dbd0794a288e
excalibur <1256953956@qq.com>
2020-04-11 18:35:47 +0800

    init

---
+++ src/main/java/lambdasinaction/Test.java
@@ -0,0 +1,9 @@
+package lambdasinaction;
+
+public class Test {
+
+  public static void main(String[] args) {
+    String[] strings = new String[] {"aaa", "bbb", "ccc", "ddd"};
+    for (String str : strings) System.out.println(str);
+  }
+}
